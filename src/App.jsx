1d363789fefaf66e0317e9f93e27b03a8bbfea2e
SDB <sdb7734@gmail.com>
2025-11-10 17:11:03 +0900

    To do 앱 탭 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import {useEffect, useState} from "react";
 import TodoItem from "./components/TodoItem.jsx";
+import {generateId} from "./utils/commons.js";
+import "./App.css";
 
 function App() {
+    const tabs = [
+        {value: 'all', label: 'All'},
+        {value: 'active', label: 'Active'},
+        {value: 'complete', label: 'Complete'},
+    ]
+
+    const [activeTab, setActiveTab] = useState("all");
+    const [todo, setTodo] = useState("")
     const [todos, setTodos] = useState([
-        {id: 1, text: '항목 1', completed: false},
-        {id: 2, text: '항목 2', completed: false},
+        {id: generateId(), text: '항목 1', completed: false},
+        {id: generateId(), text: '항목 2', completed: false},
     ]);
 
     const handleToggle = (id) => {
@@ -18,23 +28,56 @@ function App() {
         setTodos(todos.filter(todo => todo.id !== id));
     }
 
+    const handleAdd = (todo) => {
+        setTodos(
+            [...todos, {id: generateId(), text: todo, completed: false}]
+        )
+    }
+
+    const handleChange = (e) => {
+        console.log(e.target)
+        const {value} = e.target;
+        console.log(" value " + value)
+        setTodo(value);
+    }
+
+    const handleTabChange = (tab)=> {
+        setActiveTab(tab);
+    }
+
     // 렌더링이 완료된 후 실행
     useEffect(() => {
         console.log(`Todos: 변경 ${JSON.stringify(todos)}`);
     }, [todos])
 
+    useEffect(() => {
+
+    }, [activeTab])
+
     return (
         <>
-            <ul>
+            <nav className="tab-container">
+                <ul className="tab-list">
+                    {tabs.map(item => (
+                        <li key={item.value} className={`tab ${activeTab === item.value ? 'active' : ''}`} onClick={()=>handleTabChange(item?.value)} >
+                            {item.label}
+                        </li>
+                    ))
+                    }
+                </ul>
+            </nav>
+            <ul className='tab-content'>
                 {
                     todos.map((todo) => (
-                            <TodoItem key={todo.id} todo={todo} handleToggle={handleToggle} onDelete={handleDelete}/>
+                            <TodoItem key={todo.id} todo={todo} onToggle={handleToggle} onDelete={handleDelete}/>
                         )
                     )
                 }
             </ul>
+            <input name="todo" value={todo} placeholder="Add Todo" onChange={(e) => handleChange(e)}/>
+            <button onClick={() => handleAdd(todo)}>Add</button>
         </>
-    )
+    );
 }
 
 export default App
